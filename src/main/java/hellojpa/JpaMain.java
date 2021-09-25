4111228aabcdf07d1e23caaeaaf6decd35f2e2ac
김선빈 <ksb9242@gmail.com>
2021-09-25 12:17:38 +0900

    영속성 관리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,23 +21,37 @@ public class JpaMain {
         /**
          * 중요 ! JPA의 모든 데이터 변경은 트랜잭션 안에서 실행 !
          */
-        try {
-              // 비영속 상태
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
 
-              // 여기서부터 영속 상태
-              // persist를 한다해도 DB에 값이 저장되는것은 아니다.
-//            em.persist(member);
-            Member findMember = em.find(Member.class, 1L);
+        /**
+         * persist를 한다해도 DB에 값이 저장되는것은 아니다.
+         * 찾을 때 쿼리를 날리지 않고 1차 캐시에 키값(id)으로 찾아낸다.
+         * Member findMember1 = em.find(Member.class, 100L);
+         */
+        try {
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAA");
+
+            // 준영속 상태 / 영속성 컨텍스트에서 member entity 더 이상 관리 X
+            // 그러므로 위의 update 쿼리 커밋 시점에 실행 안됌
+            //em.detach(member);
+
+            //영속성 컨테스트 통으로 초기화
+            em.clear();
+
+            // clear로 1차 캐시 삭제로 인해 다시 쿼리 실행
+            Member member2 = em.find(Member.class, 150L);
+
+
+
+            System.out.println("===================");
+
             /**
              * 객체 값만 바꿔도 DB 값이 변경 되는 이유 !
              * JPA를 통해 엔티티를 가져오면 JPA가 관리를 하게 됌
              * 그 후 JPA가 변경 됐는지 안됐는지 트렌젝션 커밋 시점에 체크를 함
              * 변경을 확인하면 커밋 직전에 update쿼리를 날린다....
              */
-            findMember.setName("HelloJPA");
 
             tx.commit();
 
